598207fd254bb8e82944265dec059b6027596c36
Victor <victor.tingstrom@gmail.com>
2022-11-28 20:22:21 +0100

    Adds result object

--- jaxman/result.py
+++ jaxman/result.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List
 import jax.numpy as jnp
 
 
@@ -8,10 +9,51 @@ class Prediction(object):
     covariance: jnp.ndarray
 
 
-@dataclass
+@dataclass(frozen=True)
 class Correction(object):
     mean: jnp.ndarray
     covariance: jnp.ndarray
     gain: jnp.ndarray
     loglikelihood: jnp.ndarray
 
+
+@dataclass
+class Result(object):
+    predictions: List[Prediction] = field(default_factory=list, repr=False)
+    corrections: List[Correction] = field(default_factory=list, repr=False)
+
+    def append(self, p: Prediction, c: Correction):
+        """
+        Appends prediction and correction.
+
+        Args:
+            p (Prediction): prediction to append.
+            c (Correction): correction to append.
+        """
+
+        self.predictions.append(p)
+        self.corrections.append(c)
+
+    def filtered_means(self) -> jnp.ndarray:
+        """
+        Returns the filtered means of the result.
+
+        Returns:
+            jnp.ndarray: filtered means.
+        """
+
+        means = [c.mean for c in self.corrections][1:]
+
+        return jnp.stack(means)
+    
+    def filtered_covariances(self) -> jnp.ndarray:
+        """
+        Returns the filtered covariances of the result.
+
+        Returns:
+            jnp.ndarray: filtered covariances.
+        """
+
+        covs = [c.covariance for c in self.corrections][1:]
+
+        return jnp.stack(covs)
